04d056d25f65bc54a92cd7d893dac3b8516315fe
franfermi <franfermi@correo.ugr.es>
2019-03-05 18:37:43 +0100

    Archivo test actualizado con función de comparación de contenido y subida del PDF de prueba closes #13

--- test/test.py
+++ test/test.py
@@ -8,23 +8,26 @@ import binascii
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
+RESOURCE = './resources'
+
 class PdfReaderTestCases(unittest.TestCase):
-        def test_PdfReaderFileLoad(self):
-            with open('./resources/CalendarioExamenes18-19-GII.pdf', 'rb') as inputfile:
-                # Load PDF file from file
-                ipdf = PdfFileReader(inputfile)
-                ipdf_p1 = ipdf.getPage(0)
+    def test_PdfReaderFileLoad(self):
+        with open(os.path.join(RESOURCE, 'ejemploPDF.pdf'), mode='rb') as inputfile:
+            # Load PDF file from file
+            ipdf = PdfFileReader(inputfile)
+            ipdf_p1 = ipdf.getPage(0)
 
-                # Retrieve the text of the PDF
-                with open('./resources/out_page.txt', 'rb') as pdftext_file:
-                    pdftext = pdftext_file.read()
+            # Retrieve the text of the PDF
+            pdftext_file = open('./resources/out_page.txt', 'r')
+            pdftext = pdftext_file.read().strip()
+            pdftext_file.close()
 
-                ipdf_p1_text = ipdf_p1.extractText().replace('\n', '').encode('utf-8')
+            ipdf_p1_text = ipdf_p1.extractText().replace('\n', '').strip()
 
-                # Compare the text of the PDF to a known source
-                # self.assertEqual(ipdf_p1_text, pdftext,
-                #     msg='PDF extracted text differs from expected value.\n\nExpected:\n\n%r\n\nExtracted:\n\n%r\n\n'
-                #         % (pdftext, ipdf_p1_text))
+            # Compare the text of the PDF to a known source
+            self.assertEqual(ipdf_p1_text, pdftext,
+                msg='PDF extracted text differs from expected value.\n\nExpected:\n\n%r\n\nExtracted:\n\n%r\n\n'
+                    % (pdftext, ipdf_p1_text))
                     
 if __name__ == '__main__':
     unittest.main()
