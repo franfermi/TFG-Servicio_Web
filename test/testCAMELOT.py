742f27334525f7d4c51ecd41c31815ab5e8b0863
franfermi <franfermi@correo.ugr.es>
2019-03-20 12:35:34 +0100

    Subida final de los test de la librería CAMELOT closes #14 closes #17

--- test/testCAMELOT.py
+++ test/testCAMELOT.py
@@ -7,8 +7,12 @@ import unittest
 import camelot
 import csv
 
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+
 RESOURCE = './resources'
 OUTPUT = './outputs'
+
 
 class TestsCAMELOT(unittest.TestCase):
     def test_parsing_report(self):
@@ -19,41 +23,41 @@ class TestsCAMELOT(unittest.TestCase):
             'page': 1
         }
 
-        filename = os.path.join(RESOURCE,'foo.pdf')
+        filename = os.path.join(RESOURCE, 'foo.pdf')
         tables = camelot.read_pdf(filename)
         assert tables[0].parsing_report == parsing_report
 
     def test_PdfReaderFileLoad(self):
-        tables = camelot.read_pdf(os.path.join(RESOURCE,'foo.pdf'))
+        tables = camelot.read_pdf(os.path.join(RESOURCE, 'foo.pdf'))
 
     def test_CsvReaderFileLoad(self):
-        with open(os.path.join(OUTPUT,'foo.csv')) as File:  
+        with open(os.path.join(OUTPUT, 'foo.csv')) as File:
             reader = csv.reader(File)
 
     def test_ConvertToCsv(self):
-        tables = camelot.read_pdf(os.path.join(RESOURCE,'foo.pdf'))
-        tables.export(os.path.join(OUTPUT,'foo.csv'), f='csv', compress=False) # json, excel, html
+        tables = camelot.read_pdf(os.path.join(RESOURCE, 'foo.pdf'))
+        tables.export(os.path.join(OUTPUT, 'foo.csv'), f='csv',
+                      compress=False)  # json, excel, html
 
     def test_ComparePdfCsv(self):
-        tables = camelot.read_pdf(os.path.join(RESOURCE,'foo.pdf'))
+        tables = camelot.read_pdf(os.path.join(RESOURCE, 'foo.pdf'))
         resultsPDF = []
 
         for row in tables:
             resultsPDF.append(tables[0].df)
 
-        resultsCSV = []
-        with open(os.path.join(OUTPUT,'foo.csv')) as File:  
-            reader = csv.reader(File)
-            for row in reader:
-                resultsCSV.append(row)
-        
-        # print(resultsPDF)
-        # print('----------------------')
-        # print(resultsCSV)
+        resultsCSV = pd.read_csv(os.path.join(OUTPUT,'foo.csv'), header=-1)
+        resultsCSV_sin_nan = resultsCSV.fillna(value='')
 
-        # self.assertEqual(resultsPDF, resultsCSV,
-        #         msg='PDF extracted text differs from expected value.\n\nExpected:\n\n%r\n\nExtracted:\n\n%r\n\n'
-        #             % (resultsPDF, resultsCSV))
-                    
+        print('EXTRACT TO PDF')
+        print(tables[0].df)
+        print('----------------------')
+        print('EXTRACT TO CSV')
+        print(resultsCSV_sin_nan)
+
+        assert_frame_equal(tables[0].df, resultsCSV_sin_nan)
+
+
 if __name__ == '__main__':
     unittest.main()
+
