0e191127f171e59b598a1be16112dfd17842e910
franfermi <franfermi@correo.ugr.es>
2019-05-17 20:17:19 +0200

    Subida de los tests realizados a las funciones del bot closes #36

--- InformaticaUGR_bot/test.py
+++ InformaticaUGR_bot/test.py
@@ -2,60 +2,39 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import math
 import funcionesDB
-
-#def devuelveTrue():
-#    return True
-
-#def cuadrado(a):
-#    if(not(type(a)is int)):
-#        return -1
-#    else:
-#        return a**2;
-
-#def raiz(a):
-#    if(not(type(a)is int)):
-#        return -1
-#    elif a >= 0:
-#        return math.sqrt(a)
-#    else:
-#        raise ValueError, "a debe ser >= 0"
 
 class TestMetodos(unittest.TestCase):
 
-#    def testDevuelveTrue(self):
-#        self.assertTrue(devuelveTrue(), "Es True")
+    """ Test que comprueba que una guía docente está disponible"""
+    def test_guia_docente_disponible(self):
+        guia = funcionesDB.obtenerGuiaDocente("TFG")
+        self.assertTrue(guia)
 
-#    def testCuadrado(self):
-#        self.assertEqual(cuadrado(2), 4, "El cuadrado de 2 es 4")
-        #self.assertEqual(cuadrado(3), 6, "El cuadrado de 3 NO es 6")
+    """ Test que comprueba que una guía docente no está disponible"""
+    def test_guia_docente_no_disponible(self):
+        guia = funcionesDB.obtenerGuiaDocente("YYY")
+        self.assertFalse(guia)
 
-#    def testRaiz(self):
-#        self.assertEqual(raiz(9), 3, "La raiz de 9 es 3")
-        #self.assertEqual(raiz(-1), IndexError)
+    """ Test que comprueba que un horario está disponible"""
+    def test_horario_disponible(self):
+        horario = funcionesDB.obtenerHorarios(3, 1, "A", "Lunes")
+        self.assertTrue(horario)
 
-
-    """ Test que comprueba que una asignatura está disponible"""
-    def test_asig_disponible(self):
-        num_asig = funcionesDB.nombreAsignatura("CPD")
-        self.assertTrue(num_asig)
-
-    """ Test que comprueba que la guía docente está disponible"""
-    def test_guia_disponible(self):
-        num_guia = funcionesDB.guiaDocenteDisponible("TR")
-        self.assertTrue(num_guia)
+    """ Test que comprueba que un horario no está disponible"""
+    def test_horario_no_disponible(self):
+        horario = funcionesDB.obtenerHorarios(3, 3, "D", "Lunes")
+        self.assertFalse(horario)
 
     """ Test que comprueba que la fecha exámen está disponible"""
     def test_fechaEx_disponible(self):
-        num_fecha = funcionesDB.fechaExamenDisponible("SE")
-        self.assertTrue(num_fecha)
+        fecha = funcionesDB.obtenerFechaEx("IV", 1, "ordinaria")
+        self.assertTrue(fecha)
 
-    """ Test que comprueba el total de asignaturas disponibles"""
-    def test_numAsig_disponibles(self):
-        num_asigs = funcionesDB.numeroAsigDisponibles()
-        self.assertNotEqual(num_asigs, 0)
-
+    """ Test que comprueba que la fecha exámen no está disponible"""
+    def test_fechaEx_no_disponible(self):
+        fecha = funcionesDB.obtenerFechaEx("VV", 1, "ordinaria")
+        self.assertFalse(fecha)
 
 if __name__ == '__main__':
    unittest.main()
